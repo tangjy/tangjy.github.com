92064ceef922e3e1bd91eea241a56615d957817f
tjy <tjy@example.com>
2013-04-24 20:53:29 +0800

    add html

--- js/tjy.js
+++ js/tjy.js
@@ -1,14 +1,23 @@
 $(function() {
-    $("#hobbies a").hover(function(){
+    $("#hobbies a").hover(function() {
         $(".hobbies_detail").html($(this).find("span").text());
         $(".hobbies_detail").show();
-    }, function(){
+    }, function() {
         $(".hobbies_detail").hide();
     });
-    $("#contact a").hover(function(){
+    $("#contact a").hover(function() {
         $(".contact_detail").html($(this).find("span").text());
         $(".contact_detail").show();
-    }, function(){
+    }, function() {
         $(".contact_detail").hide();
     });
-})
+    $.extend({
+        createMail: function(tld, domain, name) {
+            var mail = "";
+            mail += 'ma' + 'il' + 'to:' + name;
+            mail += '@' + domain + '.' + tld;
+            return mail;
+        }
+    });
+    $("#email").attr("href", $.createMail("com", "gmail", "tjy430"));
+});
